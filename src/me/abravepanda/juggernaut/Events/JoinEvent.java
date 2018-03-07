60e29dae13ce7a656b6104e413e5713fcf1c354a
A_Brave_Panda <nichtompkins72@gmail.com>
2018-03-07 08:27:46 -0500

    Added few bits of documentation/comments

--- src/me/abravepanda/juggernaut/Events/JoinEvent.java
+++ src/me/abravepanda/juggernaut/Events/JoinEvent.java
@@ -17,9 +17,12 @@ public class JoinEvent implements Listener {
 		
 		Player p = e.getPlayer();
 		
+		//checks if the game is started or in progress
 		if(GameManager.gameStatus == GameProgress.INPROGRESS || GameManager.gameStatus == GameProgress.STARTING) {
+			//if so, add player as spectator
 			Main.playerManager.put(p, new PlayerManager(p.getUniqueId(), false, 0, true));
 		} else {
+			//if not, add the player to the game and update lobby count
 			Main.playerManager.put(p, new PlayerManager(p.getUniqueId(), true, 30, false));
 			Main.instance.playersInGame.add(p);
 			Main.instance.gameManager.lobbyWait(p);
